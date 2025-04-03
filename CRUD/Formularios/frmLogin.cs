fec172082102bd631dbce0d8c109cfb5a655540b
edward cabrera <edwardcabrera2323@gmail.com>
2025-04-03 13:56:30 -0400

    agregar formulario login

---
+++ CRUD/Formularios/frmLogin.cs
@@ -0,0 +1,46 @@
+﻿using CRUD.Clases;
+using CRUD.Clases.SqlClasses.LoginSL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD.Formularios
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconfirmar_Click(object sender, EventArgs e)
+        {
+            if (LoginSL.Login(Itcontrols.GetTotalTextBoxes(this))) //Valida el inicio de sesion
+            {
+                lbMessage.Text = "Login exitoso";
+            }
+            else
+            {
+                lbMessage.Text = "Usuario o contraseña incorrectos";
+                lbMessage.ForeColor = Color.Red;
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Cierra el formulario
+        {
+            Close();
+        }
+    }
+}
--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -17,7 +17,7 @@ namespace CRUD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm());
+            Application.Run(new frmLogin());
         }
     }
 }
